2d0da5e3cef0bb3e8e088726d769415046ed843d
unknown <2157489112@qq.com>
2019-05-12 18:20:32 +0800

    dengluxiamgmu

---
+++ src/LoginServlet.java
@@ -0,0 +1,105 @@
+import java.beans.Statement;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.sun.corba.se.spi.legacy.connection.Connection;
+
+public class LoginServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public LoginServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();//设置一个输出的为out的方法
+		String username = request.getParameter("username");
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = (Connection) DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/userdb", "root", "admin");//连接数据库
+			Statement sta = (Statement) ((java.sql.Connection) con).createStatement();
+			ResultSet rs = (ResultSet) ((java.sql.Statement) sta).executeQuery("SELECT user from user WHERE user='"+ username + "'");
+			if (rs.next()) {
+				request.getRequestDispatcher("/servlet/Successservlet").forward(request, response);
+			} else {
+				response.sendRedirect("/UserDengLu/Zhuce.html");
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			out.print("没找到");
+		} catch (SQLException sqle) {
+
+			out.print("连接异常");
+		}
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to
+	 * post.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 * 
+	 * @throws ServletException
+	 *             if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
